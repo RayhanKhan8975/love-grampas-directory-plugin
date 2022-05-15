01ed9688dbdd8ccd6dda93456b44c0756aeef914
Aman Khan <70699977+RayhanKhan8975@users.noreply.github.com>
2022-05-15 22:58:38 +0530

    set version number to false

--- front-end/enqueue.php
+++ front-end/enqueue.php
@@ -5,14 +5,14 @@
  * @return void
  */
 function ilg_enqueue() {
-	wp_register_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', false, time() );
-	wp_register_style( 'ilg_multiselect_css', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css', false, time() );
-	wp_register_style( 'ilg_custom_css', plugins_url( 'assets/custom.css', ILG_PATH ), false, time() );
-	wp_register_style( 'ilg_fa_css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', false, time() );
+	wp_register_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', false, false );
+	wp_register_style( 'ilg_multiselect_css', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css', false, false );
+	wp_register_style( 'ilg_custom_css', plugins_url( 'assets/custom.css', ILG_PATH ), false, false );
+	wp_register_style( 'ilg_fa_css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', false, false );
 
-	wp_register_script( 'bootstrap_js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js', array( 'jquery' ), time(), true );
-	wp_register_script( 'ilg_multi_select_js', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js', array( 'wp-i18n', 'jquery', 'bootstrap_js' ), time(), true );
-	wp_register_script( 'ilg_custom_js', plugins_url( 'assets/custom.js', ILG_PATH ), array( 'wp-i18n', 'jquery', 'ilg_multi_select_js' ), time(), true );
+	wp_register_script( 'bootstrap_js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js', array( 'jquery' ), false, true );
+	wp_register_script( 'ilg_multi_select_js', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js', array( 'wp-i18n', 'jquery', 'bootstrap_js' ), false, true );
+	wp_register_script( 'ilg_custom_js', plugins_url( 'assets/custom.js', ILG_PATH ), array( 'wp-i18n', 'jquery', 'ilg_multi_select_js' ), false, true );
 
 	wp_enqueue_style( 'bootstrap_css' );
 	wp_enqueue_style( 'ilg_multiselect_css' );
